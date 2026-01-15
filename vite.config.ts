b091a0d0ec3d75a63bb01f458e9e059aa51b63db
LIN <shenglin.sei@gmail.com>
2026-01-16 00:27:45 +0900

    fix github pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  // ⚠️ 关键：GitHub Pages 仓库名路径
+  base: '/ProjTime/',
+
   plugins: [react()],
+
   build: {
     outDir: 'docs',
     emptyOutDir: true,
